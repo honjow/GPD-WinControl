f1b18ac61ad848267906c429836788f109898d3a
honjow <honjow311@gmail.com>
2024-02-28 05:50:26 +0800

    fix install scripts

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ if [ "$EUID" -eq 0 ]
   exit
 fi
 
-cd $HOME
-
 sudo rm -rf $HOME/homebrew/plugins/GPD-WinControl
 
 echo "installing GPD-WinControl"
@@ -15,7 +13,7 @@ temp=$(mktemp -d)
 
 # GPD-WinControl
 curl -L $(curl -s https://api.github.com/repos/honjow/GPD-WinControl/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $temp/GPD-WinControl.tar.gz
-sudo tar -xzf GPD-WinControl.tar.gz -C $HOME/homebrew/plugins
+sudo tar -xzf $temp/GPD-WinControl.tar.gz -C $HOME/homebrew/plugins
 
 rm  $temp/GPD-WinControl.tar.gz
 sudo systemctl restart plugin_loader.service
